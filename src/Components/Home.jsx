08d9f1a1058a3285d0d42dd4c4ad583d377997b9
Prachi Shrestha <prachi.stha1999@gmail.com>
2022-02-27 23:57:48 +0545

    'frame-effect'

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,12 +4,12 @@ function home() {
   return (
     <div className="head container con" id='Home'>
         <div className="container">
-            <div className="row pb-5 mt-sm-0 mt-5">
+            <div className="row pb-5 my-5">
                 <div className="col-6 my-10 text-start">
                     <h1 className="text-white display-3">Hi! I am</h1>
                     <h1 className="text-white display-2 fw-bold">Prachi Shrestha</h1>
                     <a href="Prachi Shrestha-cv.pdf" download='Prachi Shrestha-cv.pdf'>
-                      <button className='btn btn-light'>Resume</button>
+                      <button className='btn text-white btn-border my-3 fs-5'>Resume</button>
                     </a>
                 </div>
             </div>
